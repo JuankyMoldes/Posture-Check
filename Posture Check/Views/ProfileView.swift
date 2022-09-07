dd1aa5ec322879da8ad6894cab8cb7ceb1144d86
Juancarlos Moldes <jcmoldes.h@gmail.com>
2022-09-07 19:27:55 -0400

    Progress Bar & Placerholder Image Ver 1.0.1
    * Added to the general structure of the Profile tab
    
    * Added progress bar
     - It needs to be updated when the user completes some questionnaires

--- Posture Check/Views/ProfileView.swift
+++ Posture Check/Views/ProfileView.swift
@@ -13,13 +13,52 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @State var progressValue: Float = 0.0
     var body: some View {
         NavigationView {
-            Text("Profile")
-                .navigationTitle("Profile")
+            ZStack{
+                
+                //Color.cyan.ignoresSafeArea()
+                
+                VStack{
+                    
+                    
+                    // This block of code below is a placeholder to be replaced later for an Image, it is unsure whether it will be a local image or uploaded by user
+                     
+                    Circle()
+                        .fill(Color.cyan)
+                        .frame(width: 150, height: 150)
+                        .position(x:  180, y: 0)
+                    
+                    // The block of code above is the code to be changed in the future
+                    
+                    
+                        Divider()
+                        .background(Color.black)
+                            .position(x: 180, y: -220)
+                    
+                }.padding()
+                
+                    VStack{
+                        
+                        // The actual progress bar needs functions which track when the user completes the steps and upates the bar so it fills up to completion
+                        
+                        ProgressBar(value:$progressValue).frame(height: 20.0)
+                            .position(x: 180, y: 130)
+                        
+                        
+                        
+                        Text("Progress 0/5")
+                            .position(x: 180, y: -150)
+                        
+                        //The progess bar Text below needs to be updated each time the user completes the required steps, a variable must be introduced which counts up +1 when what is stated above is done
+                            
+                            
+                    }.padding()
+                }
+            }
         }
     }
-}
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
